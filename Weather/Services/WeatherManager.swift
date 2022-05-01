5775326a095db556ff68479c1bdf295835e766ec
Alex Heck <h3ckman@gmail.com>
2022-05-01 15:04:59 -0400

    move apiKey to Info.plist

--- Weather/Services/WeatherManager.swift
+++ Weather/Services/WeatherManager.swift
@@ -21,13 +21,26 @@ final class WeatherManager: NSObject, ObservableObject, CLLocationManagerDelegat
         requestLocation()
     }
 
+    private var apiKey: String {
+      get {
+        guard let filePath = Bundle.main.path(forResource: "Info", ofType: "plist") else {
+          fatalError("Couldn't find file 'Info.plist'.")
+        }
+        let plist = NSDictionary(contentsOfFile: filePath)
+        guard let value = plist?.object(forKey: "API_KEY") as? String else {
+          fatalError("Couldn't find key 'API_KEY' in 'Info.plist'.")
+        }
+        return value
+      }
+    }
+    
     func fetchLocalWeather() async {
         guard let location = locationManager.location else {
             requestLocation()
             return
         }
 
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?units=\(units)&lat=\(location.coordinate.latitude)&lon=\(location.coordinate.longitude)&appid=cf002751564a4c78f5f7ed479f1b9ba3") else { return }
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?units=\(units)&lat=\(location.coordinate.latitude)&lon=\(location.coordinate.longitude)&appid=\(apiKey)") else { return }
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
             if let weatherResponse = try? JSONDecoder().decode(WeatherModel.self, from: data) {
@@ -44,7 +57,7 @@ final class WeatherManager: NSObject, ObservableObject, CLLocationManagerDelegat
         // Change query param based on type of input
         let query = (Int(cityZip) != nil) ? "zip=\(Int(cityZip)!)" : "q=\(cityZip.replacingOccurrences(of: " ", with: "+"))"
 
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?units=\(units)&\(query)&appid=cf002751564a4c78f5f7ed479f1b9ba3") else { return }
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?units=\(units)&\(query)&appid=\(apiKey)") else { return }
 
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let weather = try? JSONDecoder().decode(WeatherModel.self, from: data!) {
